da9557adb6b464777939038e3670918f0ad57756
evankennedy <evan_kennedy@yahoo.com>
2013-09-30 16:09:31 -0700

    Create AdFetcher.java

---
+++ AdFetcher.java
@@ -0,0 +1,25 @@
+/*
+ *
+ *
+ *
+ */
+
+public class AdFetcher {
+    private HashMap<String,int> stores;
+    public AdFetcher(int zip, int radius) {
+    
+    }
+    
+    
+    public DataType getCombinedAd() {
+    
+    }
+    
+    
+    private void findStores() {
+    
+    }
+    private void getAd(String store, int id) {
+    
+    }
+}
